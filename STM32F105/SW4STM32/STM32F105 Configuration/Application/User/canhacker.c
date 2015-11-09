564c7fec2744a4bc465745ff3177f9d1aa5d5513
Dmitry <dmitry@pereslegin.ru>
2015-11-10 02:26:05 +0300

    Transmit & Receive 11bit RTR

--- STM32F105/SW4STM32/STM32F105 Configuration/Application/User/canhacker.c
+++ STM32F105/SW4STM32/STM32F105 Configuration/Application/User/canhacker.c
@@ -53,6 +53,46 @@ void CanHacker_ExecGetSerial(CanHacker_HandleTypeDef *canhacker) {
     CanHacker_UartMsgReadyCallback(canhacker, str);
 }
 
+void CanHacker_ExecTransmitR11bit(CanHacker_HandleTypeDef *canhacker, uint8_t *str) {
+
+    const int idOffset = 1;
+    const int dlcOffset = 4;
+
+
+    uint8_t cmd_len = strlen((char *)str); // get command length
+
+    if (cmd_len != 5) {   // check valid cmd length
+        CanHacker_ErrorCallback(canhacker, "Error: unexpected command length");
+        return;
+    }
+
+    CanTxMsgTypeDef CAN_tx_msg;
+
+    CAN_tx_msg.RTR = CAN_RTR_REMOTE;
+    CAN_tx_msg.IDE = CAN_ID_STD;
+    // store ID
+    CAN_tx_msg.StdId = ascii2byte(str[idOffset]) << 8 | ascii2byte(str[idOffset+1]) << 4 | ascii2byte(str[idOffset+2]);
+
+    // store data length
+    //CAN_tx_msg.DLC = ascii2byte(str[dlcOffset]);
+    CAN_tx_msg.DLC = str[dlcOffset] & 0x0F;
+    // check number of data bytes supplied against data lenght byte
+    if (CAN_tx_msg.DLC != ((cmd_len - 5) / 2)) {
+        char message[80];
+        sprintf(message, "Invalid DLC: %d and %d", (int)CAN_tx_msg.DLC, (int)((cmd_len - 5) / 2));
+        CanHacker_ErrorCallback(canhacker, message);
+        return;
+    }
+
+    // check for valid length
+    if (CAN_tx_msg.DLC > 8) {
+        CanHacker_ErrorCallback(canhacker, "DLC > 8");
+        return;
+    }
+
+    CanHacker_CanTxMsgReadyCallback(canhacker, &CAN_tx_msg);
+}
+
 void CanHacker_ExecTransmit11bit(CanHacker_HandleTypeDef *canhacker, uint8_t *str) {
 
     const int idOffset = 1;
@@ -137,6 +177,10 @@ void CanHacker_Receive_Cmd(CanHacker_HandleTypeDef *canhacker, uint8_t *cmd_buf)
             CanHacker_ExecTransmit11bit(canhacker, cmd_buf);
             return;
 
+        case CANHACKER_SEND_R11BIT_ID:
+            CanHacker_ExecTransmitR11bit(canhacker, cmd_buf);
+            return;
+
             // end with error on unknown commands
         default:
             CanHacker_ErrorCallback(canhacker, "Unexpected command");
@@ -149,7 +193,11 @@ void CanHacker_Receive_Cmd(CanHacker_HandleTypeDef *canhacker, uint8_t *cmd_buf)
 
 
 void CanHacker_CanRxMsgToString(CanRxMsgTypeDef *msg, uint8_t *str) {
-    str[0] = CANHACKER_SEND_11BIT_ID;
+    if (msg->RTR == CAN_RTR_DATA) {
+        str[0] = CANHACKER_SEND_11BIT_ID;
+    } else {
+        str[0] = CANHACKER_SEND_R11BIT_ID;
+    }
     int i;
     uint32_t id = msg->StdId;
     for(i=3; i>=1; i--) {
@@ -158,12 +206,15 @@ void CanHacker_CanRxMsgToString(CanRxMsgTypeDef *msg, uint8_t *str) {
     }
     str[4] = nibble2ascii(msg->DLC);
 
-    for (i=0; i<msg->DLC; i++) {
-        uint8_t byte = msg->Data[i];
-        str[5+i*2] = nibble2ascii(byte >> 4);
-        str[5+i*2+1] = nibble2ascii(byte);
+    int length = 5;
+    if (msg->RTR == CAN_RTR_DATA) {
+        for (i=0; i<msg->DLC; i++) {
+            uint8_t byte = msg->Data[i];
+            str[5+i*2] = nibble2ascii(byte >> 4);
+            str[5+i*2+1] = nibble2ascii(byte);
+        }
+        length += msg->DLC * 2;
     }
-    int length = 5 + msg->DLC * 2;
     str[length] = CANHACKER_CR;
     str[length+1] = '\0';
 }

35f372c26d873c53c4c07331e19b2123ea8d448a
Dmitry <dmitry@pereslegin.ru>
2015-11-20 01:32:47 +0300

    TJA: Proper GPIO initialization

--- STM32F105/SW4STM32/STM32F105 Configuration/Application/User/tja1055.c
+++ STM32F105/SW4STM32/STM32F105 Configuration/Application/User/tja1055.c
@@ -6,11 +6,13 @@ void TJA1055_Init(TJA1055_HandleTypeDef *tja1055) {
     GPIO_InitStruct.Pin = tja1055->enPin;
     GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
     GPIO_InitStruct.Speed = GPIO_SPEED_LOW;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
     HAL_GPIO_Init(tja1055->enPort, &GPIO_InitStruct);
 
     GPIO_InitStruct.Pin = tja1055->stbPin;
     GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
     GPIO_InitStruct.Speed = GPIO_SPEED_LOW;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
     HAL_GPIO_Init(tja1055->stbPort, &GPIO_InitStruct);
 
     GPIO_InitStruct.Pin = tja1055->errPin;
